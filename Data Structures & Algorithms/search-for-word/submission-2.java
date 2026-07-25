7580a5e12486ef17bf1f8a62dee525008cc57ce0
Nandalal Shukla <156825592+nandalalshukla@users.noreply.github.com>
2026-07-25 18:22:43 +0530

    Add: search-for-word - submission-2

---
+++ Data Structures & Algorithms/search-for-word/submission-2.java
@@ -0,0 +1,32 @@
+class Solution {
+    private int R;
+    private int C;
+    public boolean exist(char[][] board, String word) {
+        R= board.length;
+        C=board[0].length;
+        for(int r=0;r<R;r++){
+            for(int c=0;c<C;c++){
+                if(board[r][c]==word.charAt(0)){
+                    boolean found = dfs(board, word, r,c,0);
+                    if(found) return true;
+                }
+            }
+        }
+        return false;
+    }
+    private boolean dfs(char[][] board, String word, int r, int c, int wordIndex){
+        if(wordIndex==word.length()) return true;
+        
+        if(r<0||c<0||r>=R||c>=C) return false;
+    
+        if(board[r][c]==' '|| board[r][c]!=word.charAt(wordIndex)) return false;
+        char ch = board[r][c];
+        board[r][c]=' ';
+        boolean res = dfs(board, word,r-1,c,wordIndex+1)||
+                      dfs(board, word,r,c+1,wordIndex+1)||
+                      dfs(board, word,r+1,c,wordIndex+1)||
+                      dfs(board, word,r,c-1,wordIndex+1);
+        board[r][c]=ch;
+        return res;
+    }
+}
