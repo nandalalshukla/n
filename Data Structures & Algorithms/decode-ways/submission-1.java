19ce292430c94846c1621af385781214c00bebb8
Nandalal Shukla <156825592+nandalalshukla@users.noreply.github.com>
2026-08-07 23:08:34 +0530

    Add: decode-ways - submission-1

---
+++ Data Structures & Algorithms/decode-ways/submission-1.java
@@ -0,0 +1,21 @@
+class Solution {
+    private int dfs(int i, String s, int[] dp){
+        if(dp[i]!=-1) return dp[i];
+        
+        if(s.charAt(i)=='0') return 0;
+        int res = dfs(i+1,s,dp);
+        if(i<s.length()-1){
+            if(s.charAt(i)=='1'||(s.charAt(i)=='2' && s.charAt(i+1)<'7')){
+                res+=dfs(i+2,s,dp);
+            }
+        }
+        dp[i]=res;
+        return res;
+    }
+    public int numDecodings(String s) {
+        int [] dp = new int[s.length()+1];
+        Arrays.fill(dp,-1);
+        dp[s.length()]=1;
+        return dfs(0,s,dp);
+    }
+}
