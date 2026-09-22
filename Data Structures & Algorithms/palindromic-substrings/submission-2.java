3decc0b0175ec0d26159bd392aa6435e7ee0c5ed
Nandalal Shukla <156825592+nandalalshukla@users.noreply.github.com>
2026-09-22 09:44:02 +0530

    Add: palindromic-substrings - submission-2

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-2.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int countSubstrings(String s) {
+        int c=0;
+        for(int i=0;i<s.length();i++){
+            int l=i,r=i;
+            while(l>=0 && r<s.length()&& s.charAt(l)==s.charAt(r)){
+                l--;
+                r++;
+                c++;
+
+            }
+            l=i;
+            r=i+1;
+            while(l>=0 && r<s.length() && s.charAt(l)==s.charAt(r)){
+                l--;
+                r++;
+                c++;
+            }
+
+        }
+        return c;
+    }
+}
